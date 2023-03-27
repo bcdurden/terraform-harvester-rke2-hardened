d88974498f3ca19af89d972acf5127b18f76c8ff
Brian Durden <brian.durden@rancherfederal.com>
2023-03-27 14:38:52 -0400

    first push

---
+++ provider.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    harvester = {
+      source  = "harvester/harvester"
+      version = "0.6.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.2.3"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.4.0"
+    }
+    ssh = {
+      source  = "loafoe/ssh"
+      version = "1.2.0"
+    }
+  }
+  backend "kubernetes" {
+    secret_suffix    = "state-rke2"
+    config_path      = "~/.kube/config"
+  }
+}
+
+provider "harvester" {
+}
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
