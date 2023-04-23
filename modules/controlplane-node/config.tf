fabf0597148e606268d60ccd8761a49be1751041
Brian Durden <brian.durden@rancherfederal.com>
2023-04-22 22:58:29 -0400

    updated kubevip call

--- modules/controlplane-node/config.tf
+++ modules/controlplane-node/config.tf
@@ -74,8 +74,7 @@ resource "kubernetes_secret" "cp_main_config" {
         - '--now'
         - qemu-guest-agent.service
       - INSTALL_RKE2_ARTIFACT_PATH=/var/lib/rancher/rke2-artifacts sh /var/lib/rancher/install.sh
-      - cat /var/lib/rancher/kube-vip-k3s |  vipAddress=${var.master_vip} vipInterface=${var.master_vip_interface} sh | sudo tee /var/lib/rancher/rke2/server/manifests/vip.yaml
-      - sed -ie 's|ghcr.io/kube-vip|${var.rke2_registry}/kube-vip|g' /var/lib/rancher/rke2/server/manifests/vip.yaml
+      - cat /var/lib/rancher/kube-vip-k3s |  vipAddress=${var.master_vip} vipInterface=${var.master_vip_interface} sh | sed -e 's|ghcr.io/kube-vip|${var.rke2_registry}/kube-vip|g' | sudo tee /var/lib/rancher/rke2/server/manifests/vip.yaml
       - systemctl enable rke2-server.service
       - cp -f /usr/local/share/rke2/rke2-cis-sysctl.conf /etc/sysctl.d/60-rke2-cis.conf
       - useradd -r -c "etcd user" -s /sbin/nologin -M etcd -U
